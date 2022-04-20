45315239ecf2561e1ec86300da98b4422323c472
Todor Marinov <todo@Todors-MacBook-Air.local>
2022-04-20 12:23:17 +0300

    change soft delete fields

--- childish/api_basic/models.py
+++ childish/api_basic/models.py
@@ -8,16 +8,13 @@ from datetime import datetime
 class SoftDeleteModel(models.Model):
     is_deleted = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    deleted_at = models.DateTimeField(null=True, blank=True)
 
     def soft_delete(self):
         self.is_deleted = True
-        self.deleted_at = datetime.now()
         self.save()
 
     def restore(self):
         self.is_deleted = False
-        self.deleted_at = None
         self.save()
 
     class Meta:
